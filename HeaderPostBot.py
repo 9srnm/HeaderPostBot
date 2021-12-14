0ca55e93ba5999b0495855248e9127c33cf4a108
nikskl <nikskl999@gmail.com>
2021-12-15 00:55:42 +0300

    Created the program I wanted! Works! (secured the token)

--- HeaderPostBot.py
+++ HeaderPostBot.py
@@ -1,3 +1,23 @@
 import telebot
 
-print("Hello world!")
+
+def delete_the_beginning(message: str):
+    message_list = list(map(str.strip, message.split('\n')))
+    for row in range(len(message_list)):
+        for symbol in range(len(message_list[row])):
+            if message_list[row][symbol] == ')':
+                message_list[row] = message_list[row][symbol + 1:]
+                message_list[row] = message_list[row].strip()
+                break
+    return '\n'.join(message_list)
+
+
+bot = telebot.TeleBot(token='')
+
+
+@bot.message_handler(content_types=['text'])
+def change_results_of_matches(message):
+    bot.send_message(message.from_user.id, delete_the_beginning(message.text))
+
+
+bot.polling(none_stop=True, interval=0)
